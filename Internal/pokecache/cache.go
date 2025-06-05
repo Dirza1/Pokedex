09931b93cf7a3faa60d4cd2ddcd788ed5e9e1b34
Dirza1 <jasper.olthof@xs4all.nl>
2025-06-05 09:56:30 +0200

    starting on cashe method implementation

--- Internal/pokecache/cache.go
+++ Internal/pokecache/cache.go
@@ -5,31 +5,46 @@ import (
 	"time"
 )
 
-
-
-type Cache struct{
+type Cache struct {
 	cacheEntry map[string]cacheEntry
-	mu sync.Mutex
+	mu         sync.Mutex
 }
 
-type cacheEntry struct{
+type cacheEntry struct {
 	createdAt time.Time
-	val []byte
+	val       []byte
 }
 
-
-func NewCache(key string, val []byte){
-	newCache = 
-}
-
-func cache.Add(){
+func NewCache() {
 
 }
 
-func cache.Get(){
-
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	_, exisits := c.cacheEntry[key]
+	if exisits{
+		c.cacheEntry[key].val = val
+		c.cacheEntry[key].createdAt = time.Now()
+		c.mu.Unlock()
+	} else {
+		c.cacheEntry[key] := make(cacheEntry)
+		c.cacheEntry[key].val = val
+		c.cacheEntry[key].createdAt = time.Now()
+		c.mu.Unlock()
+	}
+	
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	_, exists := c.cacheEntry[key]
+	if exists {
+		return c.cacheEntry[key].val, true
+	} else {
+		return []byte{}, false
+	}
+	
 }
 
-func cache.reapLoop(){
+func (c *Cache) reapLoop() {
 
 }
